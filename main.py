fe824859498bd07b25d1c3574716575d0487797e
kurozumi <k537056@kansai-u.ac.jp>
2021-11-22 16:45:03 +0900

    main_pcと同じところまで編集済み

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def distance(x1, x2, y1, y2):
 
 def Setting(FILENAME):
     mat = []
-    with open('/home/kurozumi/デスクトップ/benchmark/'  + FILENAME, 'r', encoding='utf-8') as fin:
+    with open('Users/kurozumi ryouho/Desktop/benchmark/'  + FILENAME, 'r', encoding='utf-8') as fin:
         for line in fin.readlines():
             row = []
             toks = line.split(' ')
@@ -54,6 +54,125 @@ def Setting(FILENAME):
 
     return Setting_Info, request_number, depo_zahyo, c, e, l, noriori
 
+
+def Route_cost(route):
+    Route_sum = 0
+    Route_sum_k = np.zeros(len(route), dtype=float, order='C')
+    for i in range(len(route)):
+        if len(route[i]) == 0:
+            Route_sum_k[i] = 0
+        else:
+            for j in range(len(route[i]) - 1):
+                Route_sum_k[i] = Route_sum_k[i] + c[route[i][j]][route[i][j + 1]]
+            Route_sum_k[i] = Route_sum_k[i] + c[0][route[i][0]]
+            Route_sum_k[i] = Route_sum_k[i] + c[0][route[i][j + 1]]
+        Route_sum = Route_sum + Route_sum_k[i]
+
+    return Route_sum
+
+
+def route_k_cost_sum(route_k):
+    route_k_sum = 0
+    for i in range(len(route_k) - 1):
+        route_k_sum = route_k_sum + c[route_k[i]][route_k[i + 1]]
+    route_k_sum = route_k_sum + c[0][route_k[0]]
+    route_k_sum = route_k_sum + c[0][route_k[i + 1]]
+
+    return route_k_sum
+
+
+def capacity(route):
+    q = np.zeros(int(m), dtype=int, order='C')
+    capacity_over = 0
+    for i in range(len(route)):
+        for j in range(len(route[i])):
+            q[i] = q[i] + noriori[route[i][j]]
+            if q[i] > Q_max:
+                capacity_over += 1
+    return capacity_over
+
+
+def capacity_route_k(route_k):
+    capacity_over = 0
+    q = 0
+    for i in range(len(route_k)):
+        q = q + noriori[route_k[i]]
+        if q > Q_max:
+            capacity_over += 1
+    return capacity_over
+
+
+def time_caluculation(Route_k):
+    B = np.zeros(n + 2, dtype=float, order='C')  # サービス開始時間（e.g., 乗せる時間、降ろす時間)
+    A = np.zeros(n + 2, dtype=float, order='C')  # ノード到着時間
+    D = np.zeros(n + 2, dtype=float, order='C')  # ノード出発時間
+    W = np.zeros(n + 2, dtype=float, order='C')  # 車両待ち時間
+    L = np.zeros(int(n / 2), dtype=float, order='C')  # リクエストiの乗車時間
+    if not len(Route_k) == 0:
+        for i in range(len(Route_k)):
+            if i == 0:
+                A[Route_k[i]] = D[i] + c[i][Route_k[i]]
+                B[Route_k[i]] = max(e[Route_k[i]], A[Route_k[i]])
+                D[Route_k[i]] = B[Route_k[i]] + d
+                W[Route_k[i]] = B[Route_k[i]] - A[Route_k[i]]
+            else:
+                A[Route_k[i]] = D[Route_k[i - 1]] + c[Route_k[i - 1]][Route_k[i]]
+                B[Route_k[i]] = max(e[Route_k[i]], A[Route_k[i]])
+                D[Route_k[i]] = B[Route_k[i]] + d
+                W[Route_k[i]] = B[Route_k[i]] - A[Route_k[i]]
+        A[-1] = D[Route_k[-1]] + c[0][Route_k[-1]]
+        B[-1] = A[-1]
+        for i in range(len(Route_k)):
+            if Route_k[i] <=  n/ 2:
+                L[Route_k[i] - 1] = B[Route_k[i] + int(n/ 2)] - D[Route_k[i]]
+    return A, B, D, W, L
+
+
+def time_window_penalty(route_k, B):  # 論文でのw(s)
+    sum = 0
+    for i in range(len(route_k)):
+        a = B[route_k[i]] - l[route_k[i]]
+        if a > 0:
+            sum = sum + a
+    a = B[-1] - l[0]
+    if a > 0:
+        sum = sum + a
+    return sum
+
+
+def ride_time_penalty(L):  # 論文でのt_s
+    sum = 0
+    for i in range(len(L)):
+        a = L[i] - L_max
+        if a > 0:
+            sum = sum + a
+    return sum
+
+def penalty_sum(route):
+    parameta = np.zeros(4)
+    c_s = Route_cost(route)
+    q_s = capacity(route)
+    d_s = 0
+    w_s = 0
+    t_s = 0
+    for i in range(len(route)):
+        if not len(route[i]) == 0:
+            ROUTE_TIME_info = time_caluculation(route[i], n)
+            d_s_s = (ROUTE_TIME_info[1][-1]-ROUTE_TIME_info[1][route[i][1]] +c[0][route[i][1]])- T_max
+            if d_s_s < 0:
+                d_s_s = 0
+            d_s = d_s + d_s_s
+            w_s = w_s + time_window_penalty(route[i], ROUTE_TIME_info[1])
+            t_s = t_s + ride_time_penalty(ROUTE_TIME_info[4])
+    no_penalty = c_s + q_s + d_s + w_s + t_s
+
+    return no_penalty
+
+def Set_Weight(alpha,beta,gamma,delta,step):
+    weight = np.array([alpha,beta**(1/step),gamma,delta])
+    return weight
+
+
 def Greedy_aco(step,nants,p):
     x_best = [[] * 1 for i in range(m)]
     Tau = np.zeros((step,n+1,n+1))
@@ -62,7 +181,7 @@ def Greedy_aco(step,nants,p):
     while True:
         if t == step:
             break
-        # Weight = ここで重みを更新（初期化）
+        weight = Set_Weight(1,0.02,100,1,t)
         # Tau(t+1) = ここでフェロモン更新
         Repeat = np.zeros((n+1,n+1)) # 初期化
         quantity = np.ones(n+1) # 初期化
@@ -71,6 +190,9 @@ def Greedy_aco(step,nants,p):
             if ant > nants:
                 break
             P = quantity/ant
+
+            x = Construction_solution(weight,Tau,Eta,Repeat,P)
+
             """
             Construction ant solution x
             decision 
@@ -80,7 +202,8 @@ def Greedy_aco(step,nants,p):
                 repeat counter R
                 quantity counter Q
             """
-
+            if penalty_sum(x) == 0 and x < x_best:
+                x_best = x
             """
             Update
                 best known solution x_best
@@ -91,7 +214,27 @@ def Greedy_aco(step,nants,p):
             ant +=1
 
         t +=1
-#def Construction_solution():
+def Construction_solution(weight,Tau,Eta,R,P):
+    x = [[] * 1 for i in range(m)]
+    """
+    移動セットAをつくる(どう作ればいいのかわからないので保留)
+    """
+    while True:
+        #以下初期化
+        #ここでx_best = []
+        S_highest = -1.
+        highest_set = np.zeros(2)
+
+        #車両_k_の現在地を取得
+        for k in range(3):
+            if len(x[k]) == 0:
+                i= 0
+            else:
+                i = x[k][-1]
+            for j in node_set:
+                pass
+
+    pass
 
 if __name__ == '__main__':
     FILENAME = 'darp01.txt'
@@ -117,4 +260,8 @@ if __name__ == '__main__':
     e = Setting(FILENAME)[4]
     l = Setting(FILENAME)[5]
 
-    Greedy_aco(5,4,3)
+    pick_up = np.arange(1,int(n/2)+1)
+    drop_off = np.arange(int(n/2)+1,n+1)
+    node_set = np.arange(1,n+1)
+    #Greedy_aco(5,4,3)
+    print(node_set)
